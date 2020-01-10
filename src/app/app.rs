e3b0a4ac41b61f0f3cc4a704f5486f2852369c44
Miad <Miad.Abrin@d2l.com>
2020-01-09 19:55:44 -0500

    remove junk

--- src/app/app.rs
+++ src/app/app.rs
@@ -1,65 +1,9 @@
-use crate::util::{RandomSignal, SinSignal, TabsState};
+use crate::util::TabsState;
 
 const TASKS: [&'static str; 24] = [
     "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10",
     "Item11", "Item12", "Item13", "Item14", "Item15", "Item16", "Item17", "Item18", "Item19",
     "Item20", "Item21", "Item22", "Item23", "Item24",
-];
-
-const LOGS: [(&'static str, &'static str); 26] = [
-    ("Event1", "INFO"),
-    ("Event2", "INFO"),
-    ("Event3", "CRITICAL"),
-    ("Event4", "ERROR"),
-    ("Event5", "INFO"),
-    ("Event6", "INFO"),
-    ("Event7", "WARNING"),
-    ("Event8", "INFO"),
-    ("Event9", "INFO"),
-    ("Event10", "INFO"),
-    ("Event11", "CRITICAL"),
-    ("Event12", "INFO"),
-    ("Event13", "INFO"),
-    ("Event14", "INFO"),
-    ("Event15", "INFO"),
-    ("Event16", "INFO"),
-    ("Event17", "ERROR"),
-    ("Event18", "ERROR"),
-    ("Event19", "INFO"),
-    ("Event20", "INFO"),
-    ("Event21", "WARNING"),
-    ("Event22", "INFO"),
-    ("Event23", "INFO"),
-    ("Event24", "WARNING"),
-    ("Event25", "INFO"),
-    ("Event26", "INFO"),
-];
-
-const EVENTS: [(&'static str, u64); 24] = [
-    ("B1", 9),
-    ("B2", 12),
-    ("B3", 5),
-    ("B4", 8),
-    ("B5", 2),
-    ("B6", 4),
-    ("B7", 5),
-    ("B8", 9),
-    ("B9", 14),
-    ("B10", 15),
-    ("B11", 1),
-    ("B12", 0),
-    ("B13", 4),
-    ("B14", 6),
-    ("B15", 4),
-    ("B16", 6),
-    ("B17", 4),
-    ("B18", 7),
-    ("B19", 13),
-    ("B20", 8),
-    ("B21", 11),
-    ("B22", 9),
-    ("B23", 3),
-    ("B24", 5),
 ];
 
 pub struct Signal<S: Iterator> {
@@ -78,21 +22,6 @@ where
         }
         self.points
             .extend(self.source.by_ref().take(self.tick_rate));
-    }
-}
-
-pub struct Signals {
-    pub sin1: Signal<SinSignal>,
-    pub sin2: Signal<SinSignal>,
-    pub window: [f64; 2],
-}
-
-impl Signals {
-    fn on_tick(&mut self) {
-        self.sin1.on_tick();
-        self.sin2.on_tick();
-        self.window[0] += 1.0;
-        self.window[1] += 1.0;
     }
 }
 
@@ -128,49 +57,17 @@ pub struct App<'a> {
     pub title: &'a str,
     pub should_quit: bool,
     pub tabs: TabsState<'a>,
-    pub progress: u16,
-    pub sparkline: Signal<RandomSignal>,
     pub tasks: ListState<&'a str>,
-    pub logs: ListState<(&'a str, &'a str)>,
-    pub signals: Signals,
-    pub barchart: Vec<(&'a str, u64)>,
     pub servers: Vec<Server<'a>>,
 }
 
 impl<'a> App<'a> {
     pub fn new(title: &'a str) -> App<'a> {
-        let mut rand_signal = RandomSignal::new(0, 100);
-        let sparkline_points = rand_signal.by_ref().take(300).collect();
-        let mut sin_signal = SinSignal::new(0.2, 3.0, 18.0);
-        let sin1_points = sin_signal.by_ref().take(100).collect();
-        let mut sin_signal2 = SinSignal::new(0.1, 2.0, 10.0);
-        let sin2_points = sin_signal2.by_ref().take(200).collect();
         App {
             title,
             should_quit: false,
             tabs: TabsState::new(vec!["Tab0", "Tab1"]),
-            progress: 0,
-            sparkline: Signal {
-                source: rand_signal,
-                points: sparkline_points,
-                tick_rate: 1,
-            },
             tasks: ListState::new(TASKS.to_vec()),
-            logs: ListState::new(LOGS.to_vec()),
-            signals: Signals {
-                sin1: Signal {
-                    source: sin_signal,
-                    points: sin1_points,
-                    tick_rate: 5,
-                },
-                sin2: Signal {
-                    source: sin_signal2,
-                    points: sin2_points,
-                    tick_rate: 10,
-                },
-                window: [0.0, 20.0],
-            },
-            barchart: EVENTS.to_vec(),
             servers: vec![
                 Server {
                     name: "NorthAmerica-1",
@@ -226,10 +123,6 @@ impl<'a> App<'a> {
     }
 
     pub fn on_tick(&mut self) {
-        // Update progress
-        self.progress += 5;
-        if self.progress > 100 {
-            self.progress = 0;
-        }
+        // Update self values
     }
 }
