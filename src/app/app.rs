646dbef27046c607c11b267357f33d37fa370c99
Miad <Miad.Abrin@d2l.com>
2020-01-09 22:35:16 -0500

    remove unneeded field

--- src/app/app.rs
+++ src/app/app.rs
@@ -25,7 +25,6 @@ impl<I> ListState<I> {
 pub struct Server<'a> {
     pub name: &'a str,
     pub location: &'a str,
-    pub coords: (f64, f64),
     pub status: &'a str,
 }
 
@@ -48,25 +47,21 @@ impl<'a> App<'a> {
                 Server {
                     name: "NorthAmerica-1",
                     location: "New York City",
-                    coords: (40.71, -74.00),
                     status: "Up",
                 },
                 Server {
                     name: "Europe-1",
                     location: "Paris",
-                    coords: (48.85, 2.35),
                     status: "Failure",
                 },
                 Server {
                     name: "SouthAmerica-1",
                     location: "São Paulo",
-                    coords: (-23.54, -46.62),
                     status: "Up",
                 },
                 Server {
                     name: "Asia-1",
                     location: "Singapore",
-                    coords: (1.35, 103.86),
                     status: "Up",
                 },
             ],
